3b8d4360e9e21433ba7536eff9f7cb3dead1ba78
Andrey Demirov <48713412+andreydemirov@users.noreply.github.com>
2019-06-10 15:37:02 +0300

    Update cpp-lab8.cpp

--- cpp-lab8/cpp-lab8.cpp
+++ cpp-lab8/cpp-lab8.cpp
@@ -1,12 +1,42 @@
-﻿// cpp-lab8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+// cpp-lab8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Вставить в список число 66 после каждого элемента с отрицательным значением
 
-#include "pch.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <list>
+#include "pch.h"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand((unsigned)time(0));
+
+	std::list<int> Li;
+	for (int i = 0; i < 50; i++)
+		Li.push_back(rand() % 100 - 50);
+
+	std::list<int>::iterator p = Li.begin();
+	for (; p != Li.end(); ++p)
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	for (p = Li.begin(); p != Li.end();)
+	{
+		if (*p < 0)
+		{
+			Li.insert(++p, 66);
+		}
+		else
+			++p;
+	}
+
+	std::cout << std::endl;
+	for (p = Li.begin(); p != Li.end(); ++p)
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	system("pause");
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
